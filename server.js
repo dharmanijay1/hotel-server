66ff00b8854c5ab03b64c242f2121d903619fde7
Dante <dharmanijay1@gmail.com>
2025-02-23 01:53:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
